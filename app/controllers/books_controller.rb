dab3a22598ed79a6ff564a2dc1a7473eedcf113d
鈴木大地 <daichi@suzukidaichinoMacBook-Pro-2.local>
2019-12-14 19:23:51 +0900

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,53 @@
 class BooksController < ApplicationController
 
   def top
-
   end
 
   def index
+  	@book = Book.new
   	@books = Book.all
   end
 
   def show
-  end
-
-  def new
-  	@book = Book.new
+  	@book = Book.find(params[:id])
   end
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
+  	 if book.save
+  	 	flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @book = book
+      @books = Book.all
+      render :index
+  	 end
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  book	 = Book.find(params[:id])
+  	if book.update(book_params)
+  		flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @book =book
+      render :edit
+  	end
+    
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
   end
 
   private
+
   def book_params
   	params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-get "root" => "books#top"
+get "top", to: "books#top"
+root to: "books#top"
 
 resources :books
 end
